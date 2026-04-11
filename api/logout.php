4d9c68447e240a46a40a41af03bd9cc3a8b46e7c
5PAC3 <matteo.on.the.sk8@gmail.com>
2026-04-11 11:22:05 +0200

    Aggiungi API logout centralizzata

---
+++ api/logout.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+
+session_start();
+session_destroy();
+
+echo json_encode(['success' => true, 'message' => 'Logout effettuato']);
+?>
