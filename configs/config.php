d619f5889f58e8004074a71e1bd6f6ef555a8857
matteo.on.the.sk8@gmail.com <matteo.on.the.sk8@gmail.com>
2026-04-15 09:33:53 +0200

    aggiunto esempio e path per windows

--- configs/config.php
+++ configs/config.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'casaautomobilistica');
 define('DB_USER', 'root');
-define('DB_PASS', 'tiamotilda');
+define('DB_PASS', '');
 
 // Abilita errori per debug (disabilitare in produzione)
 error_reporting(E_ALL);
--- js/config.template.js
+++ js/config.template.js
@@ -1,2 +1,3 @@
-const API_BASE = 'http://localhost/officina/api/';
+//onst API_BASE = 'http://localhost/officina/api/'; //linux
+const API_BASE = 'http://localhost/sarto/officina/api/'; //windows
 const DEBUG = true;
