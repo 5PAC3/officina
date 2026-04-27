c4050957e4f8b6b87e6dd6e66b1dd6c1354c9f28
sarto <matteo.on.the.sk8@gmail.com>
2026-04-27 10:12:24 +0200

    Add composer/phpdotenv for env-based config management

--- configs/config.php
+++ configs/config.php
@@ -1,10 +1,13 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'casaautomobilistica');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// Abilita errori per debug (disabilitare in produzione)
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+define('DB_HOST', $_ENV['DB_HOST'] ?? 'localhost');
+define('DB_NAME', $_ENV['DB_NAME'] ?? '');
+define('DB_USER', $_ENV['DB_USER'] ?? 'root');
+define('DB_PASS', $_ENV['DB_PASS'] ?? '');
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-?>
